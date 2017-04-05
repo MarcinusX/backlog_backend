5770206f74124447cd0fc124341ebf9687a4ddb9
Monika <monika.stobieniecka1@wp.pl>
2017-04-05 16:06:28 +0200

    Inital commit

---
+++ src/test/java/com/swimHelper/MainApplicationsTests.java
@@ -0,0 +1,16 @@
+package com.swimHelper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MainApplicationsTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
