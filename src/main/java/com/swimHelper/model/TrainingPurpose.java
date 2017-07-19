a6f232659b0fd04e2d8cfed9f159f3088e79afb3
mstobieniecka <mstobieniecka1@gmail.com>
2017-07-19 19:38:54 +0200

    Updated models #3

--- src/main/java/com/swimHelper/model/TrainingPurpose.java
+++ src/main/java/com/swimHelper/model/TrainingPurpose.java
@@ -4,5 +4,7 @@ package com.swimHelper.model;
  * Created by Monia on 2017-07-18.
  */
 public enum TrainingPurpose {
-
+    IMPROVE_RECORDS,
+    LOOSE_WEIGHT,
+    KEEP_FIT
 }
--- src/main/java/com/swimHelper/model/TrainingRequirements.java
+++ src/main/java/com/swimHelper/model/TrainingRequirements.java
@@ -1,5 +1,6 @@
 package com.swimHelper.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 import java.util.ArrayList;
@@ -9,6 +10,7 @@ import java.util.Collection;
  * Created by mstobieniecka on 2017-07-19.
  */
 @Data
+@AllArgsConstructor
 public class TrainingRequirements {
     private Collection<Style> styles = new ArrayList<>();
     private DifficultyLevel difficultyLevel;
