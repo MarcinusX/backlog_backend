170bb957f9799a25698d9abcf448b06fd229d3ce
mstobieniecka <mstobieniecka1@gmail.com>
2017-07-19 20:07:04 +0200

    Added tests for checking training requirements #3

--- src/main/java/com/swimHelper/model/StyleStatistics.java
+++ src/main/java/com/swimHelper/model/StyleStatistics.java
@@ -1,11 +1,13 @@
 package com.swimHelper.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 /**
  * Created by Monia on 2017-07-18.
  */
 @Data
+@AllArgsConstructor
 public class StyleStatistics {
     private Style style;
     private int distance;
