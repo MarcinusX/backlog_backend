fa535f3a93ee27cd0ab906150ebe0aea1ee6ed6b
mstobieniecka <mstobieniecka1@gmail.com>
2017-09-01 19:40:34 +0200

    Added update training by user (number of completed repeats and time of one repeat in exerciseSeries) #17

--- src/main/java/com/swimHelper/model/ExerciseSeries.java
+++ src/main/java/com/swimHelper/model/ExerciseSeries.java
@@ -25,4 +25,6 @@ public class ExerciseSeries {
     private int orderNumber;
     private int breakInSeconds;
     private int durationOfOneExerciseInSeconds;
+    private int completedRepeats;
+    private int averageDurationOfOneRepeatInSeconds;
 }
--- src/main/java/com/swimHelper/service/TrainingService.java
+++ src/main/java/com/swimHelper/service/TrainingService.java
@@ -1,9 +1,8 @@
 package com.swimHelper.service;
 
-import com.swimHelper.exception.MissingTrainingRequirementsException;
-import com.swimHelper.exception.UnsatisfiedTimeRequirementsException;
-import com.swimHelper.exception.UserNotFoundException;
+import com.swimHelper.exception.*;
 import com.swimHelper.generator.TrainingGenerator;
+import com.swimHelper.model.ExerciseSeries;
 import com.swimHelper.model.Training;
 import com.swimHelper.model.TrainingRequirements;
 import com.swimHelper.model.User;
@@ -13,7 +12,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.validation.ConstraintViolationException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -53,5 +54,33 @@ public class TrainingService {
             //TODO: throw Exception
         }
         return trainingRepository.saveAndFlush(training);
+    }
+
+    public Training setTrainingCompletion(Training training) throws TrainingNotFoundException, InvalidTrainingException {
+        Training trainingFromDb = trainingRepository.findOne(training.getId());
+        if (trainingFromDb != null) {
+            updateTrainingSeries(training, trainingFromDb);
+            try {
+                return trainingRepository.save(trainingFromDb);
+            } catch (ConstraintViolationException e) {
+                throw new InvalidTrainingException(e.getMessage());
+            }
+        } else {
+            throw new TrainingNotFoundException("Could not find user with id: " + training.getId());
+        }
+    }
+
+    private void updateTrainingSeries(Training training, Training trainingFromDb) {
+        List<ExerciseSeries> existingExerciseSeries = new ArrayList<>(trainingFromDb.getExerciseSeries());
+        List<ExerciseSeries> exerciseSeriesToUpdate = new ArrayList<>(training.getExerciseSeries());
+        exerciseSeriesToUpdate.forEach(series -> {
+            existingExerciseSeries.stream().filter(es -> series.getId().equals(es.getId()))
+                    .findFirst()
+                    .ifPresent(es -> {
+                                es.setAverageDurationOfOneRepeatInSeconds(series.getAverageDurationOfOneRepeatInSeconds());
+                                es.setCompletedRepeats(series.getCompletedRepeats());
+                            }
+                    );
+        });
     }
 }
